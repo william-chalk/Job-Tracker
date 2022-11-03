7592812764bd46879811d86f1b030b18bd9da4e2
Argent <jrargent42@gmail.com>
2022-11-02 20:56:59 -0400

    finalized models

--- models/index.js
+++ models/index.js
@@ -2,6 +2,30 @@ const User = require("./User");
 const Application = require("./Application");
 const Interview = require("./Interview");
 
+// create associations
+User.hasMany(Application, {
+  foreignKey: "user_id",
+});
+
+User.hasMany(Interview, {
+  foreignKey: "user_id",
+});
+
+Application.belongsTo(User, {
+  foreignKey: "user_id",
+  onDelete: "SET NULL",
+});
+
+Interview.belongsTo(User, {
+  foreignKey: "user_id",
+  onDelete: "SET NULL",
+});
+
+Interview.belongsTo(Application, {
+  foreignKey: "app_id",
+  onDelete: "SET NULL",
+});
+
 module.exports = {
   User,
   Application,
