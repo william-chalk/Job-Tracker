906d8cce2d705d2ed18872b4b9d10e46d0f25a2d
Joshua Bohde <bohdecoded@gmail.com>
2022-11-25 15:48:51 -0500

    refactor: consolidate page routes to one file and handle any page routes with data in PageController

--- controllers/PageController.js
+++ controllers/PageController.js
@@ -1,11 +1,90 @@
+const { Application, User } = require("../models");
+
 module.exports = {
-    getDashboard: (req, res) => {
-      res.render(
-        'dashboard',
+  getDashboard: (req, res) => {
+    Application.findAll({
+      where: {
+        user_id: req.session.currentUser.id,
+      },
+      attributes: [
+        "id",
+        "job_title",
+        "company_name",
+        "app_url",
+        "app_status",
+        "app_language",
+      ],
+      include: [
+      //   {
+      //     model: Interview,
+      //     attributes: ["int_time", "int_location", "int_round", "int_comments"],
+      //     include: {
+      //       model: User,
+      //       attributes: ["firstName", "lastName"],
+      //     },
+      //   },
+      // {
+      //   model: User,
+      //   attributes: ["firstName", "lastName"],
+      // },
+      ],
+    })
+      .then(dbApplicationData => {
+        const applications = dbApplicationData.map(app =>
+          app.get({ plain: true })
+        );
+        console.table(applications);
+        res.render("dashboard", { applications, isAuthenticated: req.session.isAuthenticated });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+
+  getSingleApplication: (req, res) => {
+    Application.findOne({
+      where: {
+        id: req.params.id,
+      },
+      attributes: [
+        "id",
+        "job_title",
+        "company_name",
+        "app_url",
+        "app_status",
+        "app_language",
+      ],
+      include: [
+      //   {
+      //     model: Interview,
+      //     attributes: ["int_time", "int_location", "int_round", "int_comments"],
+      //     include: {
+      //       model: User,
+      //       attributes: ["firstName", "lastName"],
+      //     },
+      //   },
         {
-          welcomeMessage: `Welcome to the dashboard ${req.session.currentUser.firstName}!`,
-          isAuthenticated: req.session.isAuthenticated
+          model: User,
+          attributes: ["firstName", "lastName"],
+        },
+      ],
+    })
+      .then(dbApplicationData => {
+        if (!dbApplicationData) {
+          res.status(404).json({ message: "No application with this id found" });
+          return;
         }
-      );
-    }
-  }
+        const application = dbApplicationData.get({ plain: true });
+
+        res.render("edit-application", {
+          application,
+          isAuthenticated: req.session.isAuthenticated,
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+};
--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,9 @@
 const router = require("express").Router();
-const { PageController } = require("../controllers/index");
 
-const isAuthenticated = require("../middleware/isAuthenticated");
-const dashboardRoutes = require("./pages/dashboard-routes");
 const apiRoutes = require("./api");
-const homeRoutes = require("./pages/home-routes");
+const pageRoutes = require("./pages");
 
-// Static pages
-router.get("/register", (req, res) => res.render("register"));
-router.get("/login", (req, res) => res.render("login"));
-
-// Pages with data
-router.get("/", isAuthenticated, PageController.getDashboard);
-
-router.use("/dashboard", dashboardRoutes);
 router.use("/api", apiRoutes);
-router.use("/", homeRoutes);
+router.use("/", pageRoutes);
 
 module.exports = router;
